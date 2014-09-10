a8221d735f40692a1b33613003967f490c08699b
Kevin Scott <kevin@scottlabsllc.com>
2014-09-10 09:00:26 -0400

    Migrate tooltipster to use the dynamic Care team layout

---
+++ public/js/care/dashboard.js
@@ -0,0 +1,39 @@
+(function($){
+	var addTooltipsToTable = function(divs) {
+		var maxHeight = parseInt($(divs[0]).css('maxHeight'),10);
+		var padding = parseInt($(divs[0]).css('padding'),10);
+		var gradientHeight = 35; // this is the shadow that peeks out from the bottom of our div
+
+		$(divs).each(function(i, div){
+			div = $(div);
+
+			var maxHeight = parseInt(div.css('maxHeight'),10);
+			var padding = parseInt(div.css('padding'),10);
+			var height = div.height();
+			var heightCutoff = maxHeight - (padding * 2) - gradientHeight;
+
+
+			if ( !div.hasClass('tooltipstered') && height >= heightCutoff ) {
+				div.tooltipster({
+					content: div.html(),
+					contentAsHTML: true,
+					maxWidth: 800
+				});
+			} else if ( div.hasClass('tooltipstered') && height < heightCutoff ) {
+				div.tooltipster('destroy');
+			}
+		});
+	};
+
+	$(document).ready(function(){
+		var divs = $('table.patient-dashboard td div');
+		if (divs.length) {
+			addTooltipsToTable(divs);
+
+			$(window).resize(function(){
+				addTooltipsToTable(divs);
+			});
+		}
+
+	});
+})(jQuery);
