bd9f3625b808dc049599f9a6a900af294fc02ed6
S5 INFOTECH <avibhamare@gmail.com>
2014-09-12 11:50:42 +0530

    public/plugins/datepicker1/

--- public/js/care/dashboard.js
+++ public/js/care/dashboard.js
@@ -1,17 +1,22 @@
 (function($){
 	var addTooltipsToTable = function(divs) {
-		var maxHeight = parseInt($(divs[0]).css('maxHeight'),10);
-		var padding = parseInt($(divs[0]).css('padding'),10);
+//		var maxHeight = parseInt($(divs[0]).css('maxHeight'),10);
+//		var padding = parseInt($(divs[0]).css('padding'),10);
 		var gradientHeight = 35; // this is the shadow that peeks out from the bottom of our div
 
 		$(divs).each(function(i, div){
 			div = $(div);
 
 			var maxHeight = parseInt(div.css('maxHeight'),10);
-			var padding = parseInt(div.css('padding'),10);
+			if(!$.isNumeric(maxHeight)) {
+				maxHeight = 10;
+			}
+			var padding = parseInt(div.css('padding-top'),10);
+			if(!$.isNumeric(padding)) {
+				padding = 10;
+			}
 			var height = div.height();
-			var heightCutoff = maxHeight - (padding * 2) - gradientHeight;
-
+			var heightCutoff = maxHeight - (padding * 2) - gradientHeight; 
 
 			if ( !div.hasClass('tooltipstered') && height >= heightCutoff ) {
 				div.tooltipster({
