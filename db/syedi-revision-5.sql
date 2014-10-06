3de5a77207cfca8bbd6430002a1e152ba39fcae1
Syedi Hasan <syedinothing@gmail.com>
2014-10-06 23:58:58 +0600

    Story: 79726482, Update: 4

--- db/syedi-revision-5.sql
+++ db/syedi-revision-5.sql
@@ -1,3 +1,4 @@
+DELETE FROM nav.appointment;
 ALTER TABLE nav.appointment ADD COLUMN caremember_name character varying(100);
 ALTER TABLE nav.appointment ADD COLUMN purpose_text character varying(200);
 ALTER TABLE nav.appointment ADD COLUMN treatment_process_step character varying(200);
