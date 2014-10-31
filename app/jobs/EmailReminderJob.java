d1758acd4ea94e78cad1cd8091d81b2c743e6cf1
S5 INFOTECH <avibhamare@gmail.com>
2014-10-31 16:11:20 +0530

    Changes for email reminder for invited members

--- app/jobs/EmailReminderJob.java
+++ app/jobs/EmailReminderJob.java
@@ -1,20 +1,38 @@
 package jobs;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import models.InvitedDTO;
+import nav.dao.InvitationDAO;
 import notifiers.Mail;
+import play.jobs.Every;
 import play.jobs.Job;
 import play.jobs.On;
+import util.EmailUtil;
 
 @On("0 0 7 * * ?")
+//@Every("1min")
 public class EmailReminderJob  extends Job {
 
 	public void doJob() {
-		System.out.println("Job : Email list population on sailthrou");
+		System.out.println("Job : Email Reminder to invited users using sailthrou");
 		runEmailJob();
 	}
-	
+
 	private boolean runEmailJob() {
-		System.out.println("Added job to send email to patient every 24 hrs or ");
-//		Mail.morningNotification();
+		//Mail.morningNotification();
+		//Moffitt-Welcome-Reminder
+		List<InvitedDTO> invitedUsers = InvitationDAO.getReminders();
+		for (InvitedDTO invitedDTO : invitedUsers) {
+			System.out.println("Email: "+invitedDTO.getEmail());
+			String url = "http://tvrhnavigator.com/";
+   		 	Map<String, Object> vars = new HashMap<String, Object>();
+   		 	vars.put("username", invitedDTO.getFirstname() + " " + invitedDTO.getLastname());
+   		 	vars.put("signupurl", url + "/invited-registration/" + invitedDTO.getId());
+   		 	EmailUtil.sendEmail(EmailUtil.MOFFITT_WELCOMEREMINDER,vars,invitedDTO.getEmail());
+		}
 		return true;
 	}
 }
