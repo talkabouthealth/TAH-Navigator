b5ae8fba03e841c41497315f1a238ae72223de41
Syedi Hasan <syedinothing@gmail.com>
2014-11-26 09:30:39 +0600

    Story: 80601908, Update: 2

--- db/syedi-revision-7.sql
+++ db/syedi-revision-7.sql
@@ -4,3 +4,5 @@ ALTER TABLE nav.patientsi ALTER COLUMN notes TYPE text;
 ALTER TABLE nav.pfupci ALTER COLUMN purpose TYPE text;
 ALTER TABLE nav.pgoal ALTER COLUMN notes TYPE text;
 ALTER TABLE nav.pconcern ALTER COLUMN notes TYPE text;
+ALTER TABLE nav.pconcern ALTER COLUMN next_step TYPE text;
+ALTER TABLE nav.pgoal ALTER COLUMN next_step TYPE text;
