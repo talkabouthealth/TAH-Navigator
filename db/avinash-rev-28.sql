64935bbf5d9213aa1abd78d73cb67e7efe44beca
avibhamare <avibhamare@gmail.com>
2015-01-13 11:34:46 +0530

    Changes to db file

--- db/avinash-rev-28.sql
+++ db/avinash-rev-28.sql
@@ -1,5 +1,7 @@
 ALTER TABLE nav.patientcareteammember ADD COLUMN isdeleted boolean;
 ALTER TABLE nav.patientcareteammember ALTER COLUMN isdeleted SET DEFAULT false;
+update nav.patientcareteammember set isdeleted = false;
 
 ALTER TABLE nav.patiencareteam ADD COLUMN isdeleted boolean;
 ALTER TABLE nav.patiencareteam ALTER COLUMN isdeleted SET DEFAULT false;
+update nav.patiencareteam set isdeleted = false;
